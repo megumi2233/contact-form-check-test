847f1235669af84854329fc1ec5fc64dcf1a361b
megumi <megumi.chocolate1209@gmail.com>
2025-09-28 16:32:01 +0900

    日本語バリデーションメッセージを仕様に合わせて追加

---
+++ src/resources/lang/ja/validation.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'required' => ':attributeを入力してください。',
+    'email' => 'メールアドレス形式で入力してください。',
+    'max' => [
+        'string' => ':attributeは:max文字以内で入力してください。',
+    ],
+    'attributes' => [
+        'last_name' => '姓',
+        'first_name' => '名',
+        'gender' => '性別',
+        'email' => 'メールアドレス',
+        'tel' => '電話番号',
+        'contact' => 'お問い合わせ内容',
+        'agreement' => '個人情報の取り扱い',
+    ],
+];
